03829a16146c2d814427895da24dd1e8f34cff81
Andrea055 <vivanco11.ac@gmail.com>
2022-08-20 20:09:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Office 2013 Pro Plus On Linux
+
+This is a script that work with Wineget and WOL to install Office 2013 on Linux through Wine.
+
+Office 2013 is the latest version that is stable in Wine. I try to make Office 2016 and superior to work stable in Wine.
+
+![This is an image](/screen.png)
---
+++ build.sh
@@ -0,0 +1,10 @@
+echo "Prepare the enviroment of WOL to support Office 2013"
+WINEARCH=win32 WINEPREFIX=/home/andreock/winetest/office2013 winetrics win7 
+WINEARCH=win32 WINEPREFIX=/home/andreock/winetest/office2013 winetricks msxml6 msftedit riched20 riched30 vb6run corefonts
+echo "Downloading Office 2013, it may take some time, please wait"
+wget https://github.com/Windows-On-Linux/Office-On-Linux/releases/download/2013/office2013.tar.gz
+echo "Extracting Office 2013, please wait"
+tar -xvzf office2013.tar.gz
+echo "Open the installer of Office 2013..."
+WINEARCH=win32 WINEPREFIX=/home/andreock/winetest/office2013 wine setup.exe
+echo "The installer have finished to work. Happy use!"
